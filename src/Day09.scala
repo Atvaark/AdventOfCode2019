bbdd753dfc01868b7eb3a01a4d0854181ae73c78
Atvaark <Atvaark@users.noreply.github.com>
2019-12-09 08:39:57 +0100

    Day09

---
+++ src/Day09.scala
@@ -0,0 +1,204 @@
+import scala.collection.mutable
+import scala.io.Source
+
+object Day09 extends App {
+  val file = Source.fromFile("./input/day09")
+  val lines = file.mkString
+  file.close()
+  val originalInstructions: Array[Long] = lines.split(",").map((str: String) => str.trim.toLong)
+
+  Loop1()
+
+  Loop2()
+
+  def Loop1(): Unit = {
+    val indexedInstructions: Array[(Long, Long)] = originalInstructions.zipWithIndex.map((tuple: (Long, Int)) => (tuple._2.toLong, tuple._1))
+    val map: mutable.Map[Long, Long] = mutable.Map.from(indexedInstructions)
+    val software = new ControlSoftware(0, map, Array(1))
+    var done = false
+    while (!done) {
+      val output = software.run()
+      output match {
+        case s: Some[Long] =>
+          println(s.value)
+        case None =>
+          done = true
+      }
+    }
+  }
+
+  def Loop2(): Unit = {
+    val indexedInstructions: Array[(Long, Long)] = originalInstructions.zipWithIndex.map((tuple: (Long, Int)) => (tuple._2.toLong, tuple._1))
+    val map: mutable.Map[Long, Long] = mutable.Map.from(indexedInstructions)
+    val software = new ControlSoftware(0, map, Array(2))
+    var done = false
+    while (!done) {
+      val output = software.run()
+      output match {
+        case s: Some[Long] =>
+          println(s.value)
+        case None =>
+          done = true
+      }
+    }
+  }
+
+  class ControlSoftware(val id: Long, var instructions: mutable.Map[Long, Long], var input: Array[Long]) {
+    private var pos: Long = 0
+    private var relativeBase: Long = 0
+
+    def run(): Option[Long] = {
+      while (instructions.contains(pos)) {
+        val instruction: Long = instructions(pos)
+        val op: Long = instruction % 100
+        //        println(instruction)
+
+        val modes = if (instruction.toString.length >= 2) {
+          instruction.toString.reverse.substring(2).map(c => c.toString.toLong).toArray
+        } else {
+          Array[Long]()
+        }
+
+        op match {
+          case 1 => // Add
+            val p1: Long = getVal(modes, pos + 1, 0)
+            val p2: Long = getVal(modes, pos + 2, 1)
+            val p3: Long = getPos(modes, pos + 3, 2)
+            //            println(p3, "=", p1, "+", p2)
+            instructions(p3) = p1 + p2
+            pos += 4
+
+          case 2 => // Mul
+            val p1: Long = getVal(modes, pos + 1, 0)
+            val p2: Long = getVal(modes, pos + 2, 1)
+            val p3: Long = getPos(modes, pos + 3, 2)
+            //            println(p3, "=", p1, "*", p2)
+            instructions(p3) = p1 * p2
+            pos += 4
+
+          case 3 => // Input
+            val p1: Long = getPos(modes, pos + 1, 0)
+            //            println(p1, "input", input)
+            instructions(p1) = input.head
+            input = input.tail
+            pos += 2
+
+          case 4 => // Output
+            val p1: Long = getVal(modes, pos + 1, 0)
+            //            println("output", p1)
+            val output: Long = p1
+            pos += 2
+            return Some(output)
+
+          case 5 => // jump-if-true
+            val p1: Long = getVal(modes, pos + 1, 0)
+            val p2: Long = getVal(modes, pos + 2, 1)
+            //            println("jump-if-true", p1, p2)
+            if (p1 != 0) {
+              pos = p2
+            } else {
+              pos += 3
+            }
+
+          case 6 => // jump-if-false
+            val p1: Long = getVal(modes, pos + 1, 0)
+            val p2: Long = getVal(modes, pos + 2, 1)
+            //            println("jump-if-false", p1, p2)
+            if (p1 == 0) {
+              pos = p2
+            } else {
+              pos += 3
+            }
+
+          case 7 => // less than
+            val p1: Long = getVal(modes, pos + 1, 0)
+            val p2: Long = getVal(modes, pos + 2, 1)
+            val p3: Long = getPos(modes, pos + 3, 2)
+            //            println("less than", p1, p2, p3)
+            if (p1 < p2) {
+              instructions(p3) = 1
+            } else {
+              instructions(p3) = 0
+            }
+            pos += 4
+
+          case 8 => // equals
+            val p1: Long = getVal(modes, pos + 1, 0)
+            val p2: Long = getVal(modes, pos + 2, 1)
+            val p3: Long = getPos(modes, pos + 3, 2)
+            //            println("equals", p1, p2, p3)
+            if (p1 == p2) {
+              instructions(p3) = 1
+            } else {
+              instructions(p3) = 0
+            }
+            pos += 4
+
+          case 9 => // adjusts the relative base
+            val p1: Long = getVal(modes, pos + 1, 0)
+            //            println("setrel", pos, "+", p1)
+            relativeBase += p1
+            pos += 2
+
+          case 99 => // Exit
+            //            println("exit")
+            return None
+
+          case _ =>
+            println("invalid opcode ", op)
+            return None
+        }
+      }
+
+      None
+    }
+
+    private def getVal(modes: Array[Long], pos: Long, modePos: Int): Long = {
+      val mode = if (modePos < modes.length) {
+        modes(modePos)
+      } else {
+        0
+      }
+
+      mode match {
+        case 0 => // Pos
+          val p: Long = instructions.getOrElse(pos, 0)
+          val v: Long = instructions.getOrElse(p, 0)
+          return v
+        case 1 => // Im
+          val v: Long = instructions.getOrElse(pos, 0)
+          return v
+        case 2 => // Relative
+          val p: Long = instructions.getOrElse(pos, 0)
+          val v: Long = instructions.getOrElse(relativeBase + p, 0)
+          return v
+        case _ =>
+          println("invalid mode ", mode)
+      }
+
+      -1
+    }
+
+    private def getPos(modes: Array[Long], pos: Long, modePos: Int): Long = {
+      val mode = if (modePos < modes.length) {
+        modes(modePos)
+      } else {
+        0
+      }
+
+      mode match {
+        case 0 => // Pos
+          val p: Long = instructions.getOrElse(pos, 0)
+          return p
+        case 2 => // Relative
+          val p: Long = instructions.getOrElse(pos, 0)
+          return relativeBase + p
+        case _ =>
+          println("invalid mode ", mode)
+      }
+
+      -1
+    }
+  }
+
+}
